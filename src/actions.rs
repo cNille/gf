e3a661f8f8eec0b3da3694254c6b98115f47a808
Christopher Nilsson <christopher.nille.nilsson@gmail.com>
2021-02-25 12:00:19 +0100

    prettify usage of regex

--- src/actions.rs
+++ src/actions.rs
@@ -8,6 +8,7 @@ pub fn print_actions () -> gf::Event {
     println!("(? for help)");
     let action : String = input().msg("Action: ").get();
 
+    // Check if command is help (`?`)
     let question_re = Regex::new(r"\s*\?\s*").unwrap();
     if  question_re.is_match(&action) { 
         println!("COMMANDS: ");
@@ -24,40 +25,48 @@ pub fn print_actions () -> gf::Event {
         return event;
     }
 
-    let git_re = Regex::new(r"g(it){0,1}\s{0,1}").unwrap();
-    let is_git_cmd = git_re.is_match(&action);
-    if  is_git_cmd {
-        // Ugly code to run a git diff
-        let re = Regex::new(r"g(it){0,1}\s{0,1}(.*)").unwrap();
-        let mut args: Vec<String> = vec![];
-        for cap in re.captures_iter(&action) {
-            let c = &cap[2];
-            args = c.split(' ').map(String::from).collect();
-            println!("ARGS: {:?}", args);
-        }
-        let output = commands::git_command(args);
-        let event = gf::Event { msg: gf::Message::COMMAND, data: output};
-        return event;
-    }
-
+    // Check if command is a diff-command
     let diff_re = Regex::new(r"d{0,1}\s*\d+").unwrap();
     let is_diff = diff_re.is_match(&action);
     if is_diff {
-        // Ugly code to run a git diff
+        // REGEX: Ugly code to run a git diff
         let re = Regex::new(r"d{0,1}\s*(\d+)").unwrap();
-        let mut index: usize = 0;
-        for cap in re.captures_iter(&action) {
-            index = cap[1].parse::<usize>().unwrap();
-        }
+        let capture = re.captures(&action).unwrap();
+        let index: usize  = capture[1].parse::<usize>().unwrap();
+
+        // Get list of files and match with index
         let status = commands::git_status();
         let lines: Vec<&str> = status.split('\n').collect();
         let mut line = lines[index].to_string();
         let file_name = line.split_off(3);
+
+        // Run diff command
         let diff_data = commands::git_diff(file_name);
+
+        // Return output 
         let event = gf::Event { msg: gf::Message::DIFF, data: diff_data};
         return event;
-    } else {
-        let event = gf::Event { msg: gf::Message::INPUT, data: action};
+    } 
+
+    // Check if is a git-command
+    let git_re = Regex::new(r"g(it){0,1}\s{0,1}").unwrap();
+    let is_git_cmd = git_re.is_match(&action);
+    if  is_git_cmd {
+        // Regex to parse command
+        let re = Regex::new(r"g(it){0,1}\s{0,1}(.*)").unwrap();
+        let capture = re.captures(&action).unwrap();
+        let c = &capture[2];
+
+        // Parse to arguments and run command
+        let args: Vec<String> = c.split(' ').map(String::from).collect();
+        println!("ARGS: {:?}", args);
+        let output = commands::git_command(args);
+
+        // Return output
+        let event = gf::Event { msg: gf::Message::COMMAND, data: output};
         return event;
     }
+
+    let event = gf::Event { msg: gf::Message::INPUT, data: action};
+    return event;
 }
